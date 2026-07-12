54ad4b4020958885c4be4c650100e351cc3ad5ba
Justin Kerr <157393319+JustinKerr401@users.noreply.github.com>
2026-07-12 19:55:27 -0400

    Added Experience webpage

--- createNavbar.js
+++ createNavbar.js
@@ -1,16 +1,19 @@
-
 const navbar = document.getElementById("navbar")
 const daSections = document.querySelectorAll("section")
 const openers = document.querySelectorAll(".opener")
 
-for (let x=0; x<daSections.length; x++){
+for (let x = 0; x < daSections.length; x++) {
     const hlink = document.createElement("a")
     hlink.classList.add("box", "autoAdded")
     hlink.href = `#${daSections[x].id}`
-    if (openers[x].textContent == "Hello! Welcome to my portfolio website."){
+
+    const title = openers[x].childNodes[0].textContent.trim();
+
+    if (title === "Hello! Welcome to my portfolio website.") {
         hlink.textContent = "Top"
     } else {
-        hlink.textContent = openers[x].textContent
+        hlink.textContent = title
     }
+
     navbar.appendChild(hlink)
 }
